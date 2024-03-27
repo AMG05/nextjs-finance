93e6758811bbf217c0b49930cb01c3f48bdbce56
Anne McGinness <anne.mcginness@stormid.com>
2024-03-27 09:37:20 +0000

    pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,12 +13,10 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   noStore();
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+  
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -58,9 +56,7 @@ export async function fetchLatestInvoices() {
 export async function fetchCardData() {
   noStore();
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
+   
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
